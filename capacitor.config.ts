a00a76b4fd1e44a3fa618a46268ff47baad1e5d7
Fmunozzafra <fmunoz@iesebre.com>
2021-11-22 19:39:00 +0100

    No aconseguia que funciones el ionic serve

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ComptadorIonic',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false 
 };
 
 export default config;
