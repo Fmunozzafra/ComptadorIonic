4f560216725a280fd26eaa79b1e0fd4061c33e95
Fmunozzafra <fmunoz@iesebre.com>
2021-11-30 15:25:44 +0100

    Prova github pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
     
       <div id="container">
         <strong>Yes: TODO AFEGIR EL BOTO DEL COMPTADOR</strong>
-        <p>PROVA<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>HOLA!<a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
   </ion-page>
